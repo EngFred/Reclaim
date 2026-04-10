ae27944f1102c8c7267766cf4639d3d742f6b78a
Engineer Fred <engfred88@gmail.com>
2026-04-10 20:56:28 +0300

    updated

--- composeApp/src/iosMain/kotlin/com/engineerfred/reclaim/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/engineerfred/reclaim/MainViewController.kt
@@ -1,5 +1,6 @@
 package com.engineerfred.reclaim
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.engineerfred.reclaim.app.ReclaimApp
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { ReclaimApp() }
